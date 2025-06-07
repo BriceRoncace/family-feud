dc8dff07d6ed24ee090c90e2028c34c3f0b08d4c
Brice Roncace <brice.roncace@gmail.com>
2025-06-07 09:53:28 -0600

    Update all_questions.js

--- js/all_questions.js
+++ js/all_questions.js
@@ -1,7 +1,16 @@
 // This is the file that contains all the question, answer and point value data.
-
-var json = [{
-	// Example Object
+var json = [
+{
+	"question": "Name something kids love about summer.",
+	"answers": ["No school", "Swimming/Pool", "Warm weather", "Ice Cream", "Vacation/Camping", "Fishing"],
+	"points": [25, 23, 17, 15, 14, 6]
+},
+{
+	"question": "Name something associated with a princess.",
+	"answers": ["Prince", "Tiara", "Disney", "Gown/Dress", "Castle", "Knight", "Frog"],
+	"points": [23, 18, 15, 15, 14, 12, 3]
+},
+{
 	"question": "Name a 'helping' profession.",
 	"answers": ["Doctor", "Nurse", "Firefighter", "Police officer", "Paramedic", "Teacher", "Soldier", "Pastor"],
 	"points": [58, 19, 10, 5, 3, 3, 1, 1]
