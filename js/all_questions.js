01829a96ef6db783bd05a5c101fc9c5cdc0762f2
Brice Roncace <brice.roncace@gmail.com>
2025-06-07 09:17:34 -0600

    Initial commit
    Clone of https://github.com/ChicagoBlend/Family-Feud

---
+++ js/all_questions.js
@@ -0,0 +1,9 @@
+// This is the file that contains all the question, answer and point value data.
+
+var json = [{
+	// Example Object
+	"question": "Name a 'helping' profession.",
+	"answers": ["Doctor", "Nurse", "Firefighter", "Police officer", "Paramedic", "Teacher", "Soldier", "Pastor"],
+	"points": [58, 19, 10, 5, 3, 3, 1, 1]
+}
+];
